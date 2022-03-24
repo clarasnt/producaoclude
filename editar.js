f03e049ba12424327540ed4d5ce40ddd3c4bf8ee
clarasnt <83384345+clarasnt@users.noreply.github.com>
2022-03-24 18:36:16 -0300

    Add files via upload

--- editar.js
+++ editar.js
@@ -31,14 +31,28 @@ function setCookie(cname,cvalue,exdays) {
 
 item.addEventListener("change", function(){
     if (item.value == "Nome"){
-        edicao1.setAttribute("type", "text")
-        edicao2.setAttribute("type", "text")
-    } else if (item.value == "Status"){
+        edicao1.outerHTML = "<input id='edicao1' type='text'>"
+        edicao2.outerHTML = "<input id='edicao2' type='text'>"
+        edicao1 = document.querySelector("#edicao1")
+        edicao2 = document.querySelector("#edicao2")
+    } else 
+    if (item.value == "Status"){
         edicao1.outerHTML = "<select id='edicao11'><option value=''>Selecionar</option><option value='Pausada'>Pausada</option><option value='Fazendo'>Fazendo</option><option value='Pronta'>Pronta</option><option value='Postada'>Postada</option><option value='Vendida'>Vendida</option></select>"
         edicao2.outerHTML = "<select id='edicao22'><option value=''>Selecionar</option><option value='Pausada'>Pausada</option><option value='Fazendo'>Fazendo</option><option value='Pronta'>Pronta</option><option value='Postada'>Postada</option><option value='Vendida'>Vendida</option></select>"
-    } else if (item.value == "Cor"){
-        edicao1.outerHTML = "<select id='edicao11'><option value=''>Selecionar</option><option value='Branco'>Branco</option><option value='Preto'>Preto</option><option value='Cinza'>Cinza</option><option value='Azul'>Azul</option><option value='Laranja'>Laranja</option></select>"
-        edicao2.outerHTML = "<select id='edicao22'><option value=''>Selecionar</option><option value='Branco'>Branco</option><option value='Preto'>Preto</option><option value='Cinza'>Cinza</option><option value='Azul'>Azul</option><option value='Laranja'>Laranja</option></select>"
+        edicao1 = document.querySelector("#edicao11")
+        edicao2 = document.querySelector("#edicao22")
+    } else
+    if (item.value == "Cor"){
+        edicao1.outerHTML = "<select id='edicao111'><option value=''>Selecionar</option><option value='Branco'>Branco</option><option value='Preto'>Preto</option><option value='Cinza'>Cinza</option><option value='Azul'>Azul</option><option value='Laranja'>Laranja</option></select>"
+        edicao2.outerHTML = "<select id='edicao222'><option value=''>Selecionar</option><option value='Branco'>Branco</option><option value='Preto'>Preto</option><option value='Cinza'>Cinza</option><option value='Azul'>Azul</option><option value='Laranja'>Laranja</option></select>"
+        edicao1 = document.querySelector("#edicao111")
+        edicao2 = document.querySelector("#edicao222")
+    } else
+    if (item.value == "Estampa"){
+        edicao1.outerHTML = "<select id='edicao1111'><option value=''>Selecionar</option><option value='Estrela'>Estrela</option><option value='Coracao'>Coracao</option><option value='Triangulo'>Triangulo</option><option value='Losangulo'>Losangulo</option><option value='Olho'>Olho</option></select>"
+        edicao2.outerHTML = "<select id='edicao2222'><option value=''>Selecionar</option><option value='Estrela'>Estrela</option><option value='Coracao'>Coracao</option><option value='Triangulo'>Triangulo</option><option value='Losangulo'>Losangulo</option><option value='Olho'>Olho</option></select>"
+        edicao1 = document.querySelector("#edicao1111")
+        edicao2 = document.querySelector("#edicao2222")
     }
 })
 
@@ -51,10 +65,27 @@ procurar.addEventListener("click", function(){
 })
 
 editar.addEventListener("click", function(){
-    var edicao11 = document.querySelector("#edicao11")
-    var edicao22 = document.querySelector("#edicao22")
-    var id = id1.value + id2.value + id3.value
-    document.querySelector("#peca").innerHTML = document.querySelector("#peca").innerHTML.replace(edicao11.value, edicao22.value)
-    var produto = document.querySelector("#produto")
-    setCookie(id+"_html", "#" + produto.innerHTML, 50)
+    if (item.value == "Status"){
+        var edicao11 = document.querySelector("#edicao11")
+        var edicao22 = document.querySelector("#edicao22")
+        var id = id1.value + id2.value + id3.value
+        document.querySelector("#peca").innerHTML = document.querySelector("#peca").innerHTML.replace(edicao11.value, edicao22.value)
+        var produto = document.querySelector("#produto")
+        setCookie(id+"_html", "#" + produto.innerHTML, 50)
+    } else if (item.value = "Cor") {
+        var edicao111 = document.querySelector("#edicao111")
+        var edicao222 = document.querySelector("#edicao222")
+        var id = id1.value + id2.value + id3.value
+        document.querySelector("#peca").innerHTML = document.querySelector("#peca").innerHTML.replace(edicao111.value, edicao222.value)
+        var produto = document.querySelector("#produto")
+        setCookie(id+"_html", "#" + produto.innerHTML, 50)
+    } else if (item.value = "Estampa") {
+        var edicao1111 = document.querySelector("#edicao1111")
+        var edicao2222 = document.querySelector("#edicao2222")
+        var id = id1.value + id2.value + id3.value
+        document.querySelector("#peca").innerHTML = document.querySelector("#peca").innerHTML.replace(edicao1111.value, edicao2222.value)
+        var produto = document.querySelector("#produto")
+        setCookie(id+"_html", "#" + produto.innerHTML, 50)
+    }
+    
 })
