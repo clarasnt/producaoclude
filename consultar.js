0feb2f5411159de0c2075245510618d214802ce6
clarasnt <83384345+clarasnt@users.noreply.github.com>
2022-03-24 02:58:29 -0300

    Add files via upload

---
+++ consultar.js
@@ -0,0 +1,27 @@
+var id1 = document.querySelector("#id1")
+var id2 = document.querySelector("#id2")
+var id3 = document.querySelector("#id3")
+var procurar = document.querySelector("#procurar")
+
+id1.addEventListener("change", function(){
+    if (id1.value == ""){
+        id3.innerHTML = "<option value=''>Selecionar</option>"
+        procurar.setAttribute("style", "opacity: 0")
+    } else
+    if (id1.value == "top"){
+        procurar.setAttribute("style", "opacity: 1")
+        id3.innerHTML = "<option value=''>Selecionar</option><option value='cs'>CS</option><option value='ct'>CT</option><option value='cp'>CP</option>"
+    } else
+    if (id1.value == "bot"){
+        procurar.setAttribute("style", "opacity: 1")
+        id3.innerHTML = "<option value=''>Selecionar</option><option value='cs'>CS</option><option value='ct'>CT</option><option value='cp'>CP</option>"
+    }
+})
+
+procurar.addEventListener("click", function(){
+    var id = id1.value+id2.value+id3.value
+    var produto = localStorage.getItem(id)
+    var elemento = document.querySelector("#produto")
+    elemento.innerHTML = produto
+    document.body.appendChild(elemento)
+})
