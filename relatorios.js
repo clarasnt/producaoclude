8661cc153d009c83d83860aa5fd1a664d9732ff9
clarasnt <83384345+clarasnt@users.noreply.github.com>
2022-03-24 20:15:07 -0300

    Add files via upload

--- relatorios.js
+++ relatorios.js
@@ -2,6 +2,7 @@ var tipo = document.querySelector("#tipo")
 var item = document.querySelector("#item")
 var nome_item = document.querySelector("#nome_item")
 var pesquisar = document.querySelector("#pesquisar")
+var tabela = document.querySelector("#tabela")
 var produtos = []
 
 tipo.addEventListener("change", function(){
@@ -24,8 +25,19 @@ tipo.addEventListener("change", function(){
 })
 
 pesquisar.addEventListener("click", function(){
+    tabela.innerHTML = "<tr><td>ID</td><td>Nome</td><td>Cor</td><td>Estampa</td></tr>"
     var nome_cookie = document.cookie.split("; ")
     for (var i = 1; i <= nome_cookie.length/2; i++){
         produtos.push(JSON.parse(nome_cookie.find(row => row.startsWith("top" + i + "cs=")).split("=")[1]))
     }
+    var escolhidos = produtos.filter(function(element){
+        return element.status == item.value
+    })
+    escolhidos.forEach(function(element){
+        if (tipo.value = "Status"){
+            var linha = document.createElement("tr")
+            linha.innerHTML = "<td>" + element.id + "</td><td>" + element.nome + "</td><td>" + element.cor +"</td><td>" + element.estampa + "</td>"
+        }
+    })
+    
 })
