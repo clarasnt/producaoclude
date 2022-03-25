e09dcab1c1d0666292773feb625a264178a70e05
clarasnt <83384345+clarasnt@users.noreply.github.com>
2022-03-25 05:24:45 -0300

    Update consultar.js

--- consultar.js
+++ consultar.js
@@ -25,16 +25,3 @@ procurar.addEventListener("click", function(){
     elemento.innerHTML = "<div id='peca'><p>ID: " + produto.id + "</p><p>Nome: " + produto.nome + "</p><p>Status: " + produto.status + "</p><p>Cor: " + produto.cor + "</p><p>Estampa: " + produto.estampa + "</p><p>MEDIDAS</p><p>Comprimento: " + produto.comprimento + "</p><p>Ombro a Ombro: " + produto.ombro + "</p><p>Busto: " + produto.busto + "</p><p>Cintura: " + produto.cintura + "</p><p>Comprimento da Manga: " + produto.manga + "</p><p>Cava: " + produto.cava + "</p></div>"
     document.body.appendChild(elemento)
 })
-
-var imprimir = document.querySelector("#imprimir")
-imprimir.addEventListener("click", function(){
-    id1 = document.querySelector("#id1")
-    id2 = document.querySelector("#id2")
-    id3 = document.querySelector("#id3")
-    var id = id1.value+id2.value+id3.value
-    var produto = JSON.parse(document.cookie.split("; ").find(row => row.startsWith(id)).split("=")[1])
-    var doc = new jsPDF()
-    doc.setFontSize(15)
-    doc.text('Nome: '+produto.nome+"\nCor: "+produto.cor+"\nEstampa: "+produto.estampa+"\nMEDIDAS\nComprimento: "+produto.comprimento+"\nOmbro a Ombro: "+produto.ombro+"\nBusto: "+produto.busto+"\nCintura: "+produto.cintura+"\nComprimento da Manga: "+produto.manga+"\nCava: "+produto.cava, 10, 10)
-    doc.save(produto.nome + '.pdf')
-})
